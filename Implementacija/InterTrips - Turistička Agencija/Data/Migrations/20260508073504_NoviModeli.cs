a7c381ec512f59ea1734b5626a487573af5767ff
aobhodas2 <aobhodas2@etf.unsa.ba>
2026-05-08 10:38:37 +0200

    Pocetak implementacije

---
+++ Implementacija/InterTrips - Turistička Agencija/Data/Migrations/20260508073504_NoviModeli.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterTrips___Turistička_Agencija.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NoviModeli : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
