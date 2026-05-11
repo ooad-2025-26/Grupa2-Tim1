455a461632d10abaebe06f52591241c6dfcf5f8e
aobhodas2 <aobhodas2@etf.unsa.ba>
2026-05-11 13:40:59 +0200

    lozinka

--- Implementacija/InterTrips - Turistička Agencija/Controllers/NewsletterController.cs
+++ Implementacija/InterTrips - Turistička Agencija/Controllers/NewsletterController.cs
@@ -4,6 +4,13 @@ using System.Net.Mail;
 
 public class NewsletterController : Controller
 {
+    private readonly IConfiguration _config;
+
+    public NewsletterController(IConfiguration config)
+    {
+        _config = config;
+    }
+
     [HttpPost]
     public async Task<IActionResult> Subscribe(string email)
     {
@@ -11,42 +18,28 @@ public class NewsletterController : Controller
 
         try
         {
-            // TVOJI PODACI
-            string mojEmail = "amelaobhodas2@gmail.com"; 
-            string lozinka = "bpryrmvxwohfzodx"; // Tvoj App Password (bez razmaka)
+            var smtpServer = _config["EmailSettings:SmtpServer"];
+            var port = int.Parse(_config["EmailSettings:Port"]);
+            var senderEmail = _config["EmailSettings:SenderEmail"];
+            var appPassword = _config["EmailSettings:AppPassword"];
+            var senderName = _config["EmailSettings:SenderName"];
 
-            using (var smtpClient = new SmtpClient("smtp.gmail.com"))
+            using (var smtpClient = new SmtpClient(smtpServer))
             {
-                smtpClient.Port = 587;
+                smtpClient.Port = port;
                 smtpClient.EnableSsl = true;
-                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
-                smtpClient.UseDefaultCredentials = false; 
-                smtpClient.Credentials = new NetworkCredential("amelaobhodas2@gmail.com", "bpryrmvxwohfzodx");
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(senderEmail, appPassword);
 
                 var mailMessage = new MailMessage
                 {
-                    From = new MailAddress(mojEmail, "InterTrips Tim"),
+                    From = new MailAddress(senderEmail, senderName),
                     Subject = "InterTrips - prijava na newsletter",
-                    Body = $@"
-                    <div style='font-family: Arial, sans-serif; color: #333;'>
-                        <h2>Pozdrav!</h2>
-                        <p>Zahvaljujemo se na ispunjenoj newsletter formi.</p>
-                        <hr>
-                        <p><strong>Zapratite nas:</strong></p>
-                        <p>Ukoliko imate pitanja ili trebate dodatne informacije, kontaktirajte naš ured:</p>
-                        <p>
-                            <strong>InterTrips d.o.o.</strong><br>
-                            Ferhadija 12<br>
-                            71000 Sarajevo, BiH<br>
-                            <a href='https://intertrips.ba' style='color: #2f7d86;'>intertrips.ba</a><br>
-                            info@intertrips.ba
-                        </p>
-                    </div>",
+                    Body = "<h2>Pozdrav!</h2><p>Zahvaljujemo se na prijavi.</p>", 
                     IsBodyHtml = true
                 };
 
                 mailMessage.To.Add(email);
-
                 await smtpClient.SendMailAsync(mailMessage);
             }
 
@@ -54,8 +47,7 @@ public class NewsletterController : Controller
         }
         catch (Exception ex)
         {
-            System.Diagnostics.Debug.WriteLine("GREŠKA: " + ex.Message);
-            return StatusCode(500, ex.Message);
+            return StatusCode(500, "Došlo je do greške pri slanju.");
         }
     }
 }
