d1600c62b46b1d49fc2aa16ccd4738f89f2dcf7d
bbikic1 <benjaminbikic45@gmail.com>
2026-05-23 23:00:37 +0200

    Dodavanje hotela za admina, popravljen DB password i prikaz letova u rezervaciji

--- Implementacija/InterTrips - Turistička Agencija/Controllers/HoteliController.cs
+++ Implementacija/InterTrips - Turistička Agencija/Controllers/HoteliController.cs
@@ -1,12 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
+using InterTrips___Turistička_Agencija.Models;
+using InterTrips___Turistička_Agencija.Data;
 
 namespace InterTrips___Turistička_Agencija.Controllers
 {
     public class HoteliController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HoteliController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+        
             return View();
+        }
+
+    
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Hotel hotel)
+        {
+        
+            if (ModelState.IsValid)
+            {
+               
+                _context.Add(hotel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(hotel);
         }
     }
 }
